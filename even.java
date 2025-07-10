01b62f5cc214dff131f76ecd54a4fe3bfb31c8ba
devilhunter005 <devilhunter12990@gmail.com>
2025-07-10 14:49:33 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,16 @@
+package series.java;
+
+public class even_num_between_50_to_100
+{
+	public static void main(String[] args)
+	{
+		for(int i =50;i<=100;i++)
+		{
+			if(i%2==0) 
+			{
+			System.out.print(" i= "+i+" ");
+			}
+			}
+	}
+	
+}
